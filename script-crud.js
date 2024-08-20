c8d302dda8b95856c263759c9c24bb9bf9add70c
Harland Lohora <harland@lohora.com>
2024-08-19 22:35:10 -0600

    clase02 completada

--- script-crud.js
+++ script-crud.js
@@ -1,3 +1,13 @@
 "use strict";
 const btnAgregarTarea = document.querySelector(".app__button--add-task");
-btnAgregarTarea.type = "button";
+const formAgregarTarea = document.querySelector(".app__form-add-task");
+const textarea = document.querySelector(".app__form-textarea");
+const ulTareas = document.querySelector(".app__section-task-list");
+const pDescTarea = document.querySelector(".app__section-active-task-description");
+const removerConcluidasBtn = document.getElementById("btn-remover-concluidas");
+const removerTodasBtn = document.getElementById("btn-remover-todas");
+let tareas = JSON.parse(localStorage.getItem("tareas") || '""') || [];
+tareas.forEach((tarea) => {
+    console.log(tarea.descripcion);
+});
+//any --> cualquiera
--- script-crud.ts
+++ script-crud.ts
@@ -1,5 +1,22 @@
 const btnAgregarTarea = document.querySelector(".app__button--add-task") as HTMLButtonElement
+const formAgregarTarea = document.querySelector(".app__form-add-task") as HTMLFormElement
+const textarea = document.querySelector(".app__form-textarea") as HTMLTextAreaElement
+const ulTareas = document.querySelector(".app__section-task-list") as HTMLUListElement
+const pDescTarea = document.querySelector(".app__section-active-task-description") as HTMLParagraphElement
+const removerConcluidasBtn = document.getElementById("btn-remover-concluidas") as HTMLButtonElement
+const removerTodasBtn = document.getElementById("btn-remover-todas") as HTMLButtonElement
 
-btnAgregarTarea.type = "button"
+//interface  --> interfaz
 
+interface Tarea {
+    descripcion: string
+    complete?: boolean
+}
 
+let tareas: Tarea[] = JSON.parse(localStorage.getItem("tareas") || '""') || []
+
+tareas.forEach((tarea) => {
+    console.log(tarea.descripcion)
+})
+
+//any --> cualquiera
