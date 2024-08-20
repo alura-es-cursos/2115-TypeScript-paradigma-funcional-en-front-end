41d4140252124ec5706e26c9405f7c34d7e02fd5
Harland Lohora <harland@lohora.com>
2024-08-19 19:42:46 -0600

    clase01 completada

---
+++ script-crud.ts
@@ -0,0 +1,5 @@
+const btnAgregarTarea = document.querySelector(".app__button--add-task") as HTMLButtonElement
+
+btnAgregarTarea.type = "button"
+
+
---
+++ test.js
@@ -0,0 +1,15 @@
+"use strict";
+const nombre = "harland";
+const apellido = "lohora";
+const usuario = {
+    nombre: "Harland",
+    apellido: "Lohora",
+    edad: 28,
+    soltero: true,
+    comidasFavoritas: ["Tlayuda", "Molotes", "Tacos al pastor"]
+};
+console.log(usuario.apellido.length);
+//legnth lenght length 
+if (usuario.edad >= 18) {
+    console.log("Permitido");
+}
